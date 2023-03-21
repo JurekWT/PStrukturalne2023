4c5a448eb98136b1ce2935876b4445b6682bd309
Jerzy Tymofiejczyk <128184858+JurekWT@users.noreply.github.com>
2023-03-21 20:13:29 +0100

    dokończone zadanie

--- Zadania 3/Zadanie 3/main.cpp
+++ Zadania 3/Zadanie 3/main.cpp
@@ -2,15 +2,26 @@
 
 int pierwsza_cyrfa(int a) {
     int p;
-    while (a>=10){
-        a = a /10;
-    }
+    while (a >= 10)
+        a = a / 10;
+
     p = a;
     return p;
 }
 
 int ostatnia_cyfra(int a) {
+    int o;
+    o = a % 10;
+    return o;
+}
 
+int ilosc_cyfr(int a) {
+    int ilosc =1;
+    while (a>=10) {
+        a=a/10;
+        ilosc++;
+    }
+    return ilosc;
 }
 
 int main() {
@@ -18,5 +29,7 @@ int main() {
     int a;
     scanf("%d",&a);
     printf("Pierwsza cyfra: %d\n", pierwsza_cyrfa(a));
+    printf("Ostatnia cyfra: %d\n", ostatnia_cyfra(a));
+    printf("Ilosc cyfr: %d", ilosc_cyfr(a));
     return 0;
 }
